958fbade80d46e0e4d8bdbad981b12538cb9b1da
Rodri Quelali <rodriquelali@gmail.com>
2024-03-01 09:28:23 -0400

    operadores

--- 01-type/07-funcionesFlecha.php
+++ 01-type/07-funcionesFlecha.php
@@ -1,6 +1,18 @@
 <?php
 
-//lo mas actualizado de php en funciones
+//lo mas actualizado de php en funcion flecha
+/**
+ * las funciones con flechas fueron incorporados en la
+ * version de 8
+ * sintaxi:
+ * fn() => return;
+ * FUNCIONES ANONIMAS
+ * $suma = fn() => return;
+ */
+//forma tradicional
+ //$suma = function {
+ //}
+$suma = fn($a, $b) => $b+$a;
 
-$funcio = fn($a) $a; 
-$funcio;
+
+echo $suma(3,4);
